6ffff4a732f1f4dc7692d9d8068489bb884d4f04
Rudovsky Egor <rudovskiy45@mail.ru>
2024-02-18 01:21:34 +0500

    Время разбивать камни

---
+++ src/presenter/trip-events-presenter.js
@@ -0,0 +1,26 @@
+import SortView from '../view/sort-view.js';
+import ListView from '../view/list-view.js';
+import NewPointView from '../view/new-point-view.js';
+import EditablePointView from '../view/editable-point-view.js';
+import PointView from '../view/point-view.js';
+
+import {render} from '../render.js';
+
+export default class TripEventsPresenter {
+  listComponent = new ListView();
+
+  constructor({tpipEventsContainer}) {
+    this.tpipEventsContainer = tpipEventsContainer;
+  }
+
+  init() {
+    render(new SortView(), this.tpipEventsContainer);
+    render(this.listComponent, this.tpipEventsContainer);
+    render(new EditablePointView, this.listComponent.getElement());
+    render(new NewPointView(), this.listComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new PointView(), this.listComponent.getElement());
+    }
+  }
+}
